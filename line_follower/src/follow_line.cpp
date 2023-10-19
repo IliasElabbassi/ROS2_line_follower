e9ac77951e141a194f0290808d00dfa64a12a3be
ilias el abbassi <iliaselabbassi@outlook.fr>
2023-10-19 11:57:07 +0200

    feat: pid control server service

--- line_follower/src/follow_line.cpp
+++ line_follower/src/follow_line.cpp
@@ -1,10 +1,12 @@
 #include <memory>
+#include <cstdlib>
 #include <chrono>
 #include <cmath>
 
 #include "rclcpp/rclcpp.hpp"
 #include "geometry_msgs/msg/twist.hpp"
 #include "line_follower_interfaces/msg/contour.hpp"
+#include "line_follower_interfaces/srv/angle.hpp"
 
 #include <opencv2/opencv.hpp>
 
@@ -18,15 +20,15 @@ class FollowLine : public rclcpp::Node
 
             CmdVelpublisher_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
             ContourSubscription = this->create_subscription<line_follower_interfaces::msg::Contour>("/contourMoment", 10, std::bind(&FollowLine::handleMovement, this, _1));
+            PID_client = this->create_client<line_follower_interfaces::srv::Angle>("getPidOutput");
         }
 
     private:
         rclcpp::Subscription<line_follower_interfaces::msg::Contour>::SharedPtr ContourSubscription;
         rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr CmdVelpublisher_;
-
+        rclcpp::Client<line_follower_interfaces::srv::Angle>::SharedPtr PID_client;
+        
         void handleMovement(const line_follower_interfaces::msg::Contour::SharedPtr contour_moment_msg) const{
-            // RCLCPP_INFO(this->get_logger(), "Movement Handling : centroid (%d, %d) !", contour_moment_msg->centroid_x, contour_moment_msg->centroid_y);
-
             // {hypotenuse, adjacent, opposée}
             std::vector<int> vector_distance = compute_distances(contour_moment_msg);
 
@@ -38,17 +40,11 @@ class FollowLine : public rclcpp::Node
                 std::cout << "side : right" << std::endl;
             }
 
-            // for(auto val : vector_distance) {
-            //     std::cout << val << " ";
-            // }
-            // std::cout << std::endl;
-            
             int opposee = vector_distance[2];
             int hypotenuse = vector_distance[0];
 
-            // std::cout << "o : " << opposee << "h : " << hypotenuse << std::endl;
-
             double angle = compute_angle_degree(opposee, hypotenuse);
+            // pid_request(angle, side);
 
             std::cout << "angle CBD : " << angle << std::endl;
         }
@@ -58,34 +54,21 @@ class FollowLine : public rclcpp::Node
             A.x = contour_moment_msg->image_width / 2;
             A.y = 0;
 
-            // std::cout << "A : (" << A.x << "," << A.y << ")" << std::endl;
-
             B.x = contour_moment_msg->image_width / 2;
             B.y = contour_moment_msg->image_height;
-
-            // std::cout << "B : (" << B.x << "," << B.y << ")" << std::endl;
 
             C.x = contour_moment_msg->centroid_x;
             C.y = contour_moment_msg->centroid_y;
 
-            // std::cout << "C : (" << C.x << "," << C.y << ")" << std::endl;
-
             D.x = B.x;
             D.y = C.y;
 
-            // std::cout << "D : (" << D.x << "," << D.y << ")" << std::endl;
-
             // hypotenuse
             int distance_BC = compute_length(B, C);
-            // std::cout << "BC : " << distance_BC << std::endl;
-
             // adjacent
             int distance_BD = compute_length(B, D);
-            // std::cout << "BD : " << distance_BD << std::endl;
-
             /// opposée
             int distance_CD = compute_length(C, D);
-            // std::cout << "CD : " << distance_CD << std::endl;
 
             std::vector<int> distance_vector;
 
@@ -102,9 +85,6 @@ class FollowLine : public rclcpp::Node
 
         double compute_angle_degree(const int distance1, const int distance2) const {
             double param = (double) distance1 / distance2;
-
-            // std::cout << "in compute angle : " << distance1 << " / " << distance2 << "= " << param << std::endl;
-
             return (std::asin(param)*180/3.1415);
         }
 
@@ -123,14 +103,40 @@ class FollowLine : public rclcpp::Node
             return 1; // centroid in the left of the camera
         }
 
+        int pid_request(double angle, int side) const {
+            auto request = std::make_shared<line_follower_interfaces::srv::Angle::Request>();
+            request->angle = angle;
+            request->side = side;
+            
+            std::chrono::seconds one_second(1);
+
+            if(!PID_client->wait_for_service(one_second)){
+                if (!rclcpp::ok()) {
+                    RCLCPP_ERROR(this->get_logger(), "Interrupted while waiting for the service. Exiting.");
+                    return 0;
+                }
+                RCLCPP_INFO(this->get_logger(), "PID service not available, waiting again...");
+            }
+
+            // Make a service request
+            auto result = PID_client->async_send_request(request);
+
+            // Process the response
+            result.wait();
+
+            if (result.get()) {
+                RCLCPP_INFO(this->get_logger(), "Service call was successful. Result: %f", result.get()->pid_output);
+            } else {
+                RCLCPP_ERROR(this->get_logger(), "Service call failed.");
+            }
+        }
+
         void move(){
             geometry_msgs::msg::Twist twist_msg;
             twist_msg.linear.x = 0.2; // Move forward at 0.5 m/s
             twist_msg.angular.z = 0.2; // Rotate at 0.1 rad/s
 
             CmdVelpublisher_->publish(twist_msg);
-
-            // RCLCPP_INFO(this->get_logger(), "Publishing cmd vel");
         }
 };
 
---
+++ line_follower/src/pid_control_server.cpp
@@ -0,0 +1,43 @@
+
+#include <opencv2/opencv.hpp>
+
+#include "rclcpp/rclcpp.hpp"
+#include "line_follower_interfaces/srv/angle.hpp"
+
+class PID_controler : public rclcpp::Node
+{
+public:
+    PID_controler() : Node("pid_controller") {
+        RCLCPP_INFO(this->get_logger(), "Launched PID controler Node !");
+        PIDservice = this->create_service<line_follower_interfaces::srv::Angle>("getPidOutput",  std::bind(&PID_controler::calculateControlOutput, this, std::placeholders::_1, std::placeholders::_2));
+    }
+
+private:
+    rclcpp::Service<line_follower_interfaces::srv::Angle>::SharedPtr PIDservice;
+    
+    double kp_ = 1;
+    double ki_ = 1;
+    double kd_ = 1.0;
+    double setpoint_ = 0.0;
+
+    double integral_ = 0.0;
+    double previous_error_ = 0.0;
+
+    void calculateControlOutput(const std::shared_ptr<line_follower_interfaces::srv::Angle::Request> request, 
+                                        std::shared_ptr<line_follower_interfaces::srv::Angle::Response> response) {
+        integral_ += request->angle;
+        double derivative = request->angle - previous_error_;
+        double control_output = kp_ * request->angle + ki_ * integral_ + kd_ * derivative;
+        previous_error_ = request->angle;
+
+        response->pid_output = control_output;
+    }
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<PID_controler>());
+  rclcpp::shutdown();
+  return 0;
+}
